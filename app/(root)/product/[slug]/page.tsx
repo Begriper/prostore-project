ab551182008d169e4a12dc5660a3ada9915a8b45
Begriper <fero.stolar@gmail.com>
2025-04-09 12:57:11 +0200

    Add to cart component

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,7 +1,7 @@
+import AddToCart from "@/components/shared/product/add-to-cart";
 import ProductImages from "@/components/shared/product/product-images";
 import ProductPrice from "@/components/shared/product/product-price";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
@@ -70,9 +70,18 @@ const ProductDetailsPage = async (props: {
                                     )}
                                 </div>
                                 {product.stock > 0 && (
-                                    <Button className="w-full">
-                                        Add To Cart
-                                    </Button>
+                                    <div className="flex-center">
+                                        <AddToCart
+                                            item={{
+                                                productId: product.id,
+                                                name: product.name,
+                                                slug: product.slug,
+                                                price: String(product.price),
+                                                qty: 1,
+                                                image: product.images![0],
+                                            }}
+                                        />
+                                    </div>
                                 )}
                             </CardContent>
                         </Card>
