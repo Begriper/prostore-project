6f8fac4499f13d08c2b714baed3c6b9a43371c73
Begriper <fero.stolar@gmail.com>
2025-04-10 19:12:16 +0200

    Remove from cart and dynamic button

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
+import { getMyCart } from "@/lib/actions/cart.actions";
 
 const ProductDetailsPage = async (props: {
     params: Promise<{ slug: string }>;
@@ -13,6 +14,8 @@ const ProductDetailsPage = async (props: {
 
     const product = await getProductBySlug(slug);
     if (!product) notFound();
+
+    const cart = await getMyCart();
 
     return (
         <>
@@ -72,6 +75,7 @@ const ProductDetailsPage = async (props: {
                                 {product.stock > 0 && (
                                     <div className="flex-center">
                                         <AddToCart
+                                            cart={cart}
                                             item={{
                                                 productId: product.id,
                                                 name: product.name,
