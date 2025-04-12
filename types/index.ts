098cd4068f1c24e0f9518b618c6729eb36b311b5
Begriper <fero.stolar@gmail.com>
2025-04-12 15:49:42 +0200

    Shipping address zod schema, type and page

--- types/index.ts
+++ types/index.ts
@@ -1,16 +1,18 @@
 import {
-    cartItemSchema,
-    insertCartSchema,
-    insertProductSchema,
+  cartItemSchema,
+  insertCartSchema,
+  insertProductSchema,
+  shippingAddressSchema,
 } from "@/lib/validators";
 import { z } from "zod";
 
 export type Product = z.infer<typeof insertProductSchema> & {
-    id: string;
-    rating: string;
-    numReviews: number;
-    createdAt: Date;
+  id: string;
+  rating: string;
+  numReviews: number;
+  createdAt: Date;
 };
 
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
