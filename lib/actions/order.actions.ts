3f54dfb8dd92e8801eb19711c9f11b547a51381e
Begriper <fero.stolar@gmail.com>
2025-04-22 17:03:51 +0200

    Admin overview data & chart

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -306,8 +306,11 @@ export async function getOrderSummary() {
   // Get monthly sales
   const salesDataRaw = await prisma.$queryRaw<
     Array<{ month: string; totalSales: Prisma.Decimal }>
-  >`SELECT to_char("createdAt", 'MM/YY' as "month", sum("totalPrice") as "totalSales") FROM
-"Order" GROUP BY to_char("createdAt", 'MM/YY')`;
+  >`SELECT 
+    to_char("createdAt", 'MM/YY')    AS "month", 
+    sum("totalPrice")               AS "totalSales"
+  FROM "Order"
+  GROUP BY to_char("createdAt", 'MM/YY')`;
 
   const salesData: SalesDataType = salesDataRaw.map((entry) => ({
     month: entry.month,
