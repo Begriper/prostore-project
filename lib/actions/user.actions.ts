ebc06a1361ef201501040ba14d1a5a30bbc4bc3c
Begriper <fero.stolar@gmail.com>
2025-04-26 19:33:35 +0200

    Update User Action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -9,6 +9,7 @@ import {
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
+  updateUserSchema,
 } from "@/lib/validators";
 import { ShippingAddress } from "@/types";
 import { hashSync } from "bcrypt-ts-edge";
@@ -212,5 +213,27 @@ export async function deleteUser(id: string) {
       success: false,
       message: formatError(error),
     };
+  }
+}
+
+// Update a user
+export async function updateUser(user: z.infer<typeof updateUserSchema>) {
+  try {
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        name: user.name,
+        role: user.role,
+      },
+    });
+
+    revalidatePath("/admin/users");
+
+    return {
+      success: true,
+      message: "User updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
   }
 }
