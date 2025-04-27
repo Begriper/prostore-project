250d34e867b8c0186484552c0fe9df3785da4ad9
Begriper <fero.stolar@gmail.com>
2025-04-27 16:18:39 +0200

    Featured Products Carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,27 @@
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from "@/lib/actions/product.actions";
 import { Product } from "@/types";
 
 const Homepage = async () => {
-    const latestProducts = await getLatestProducts();
+  const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
-    return (
-        <>
-            <ProductList
-                data={latestProducts as unknown as Product[]}
-                title="Newest Arrivals"
-                limit={4}
-            />
-        </>
-    );
+  return (
+    <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+      <ProductList
+        data={latestProducts as unknown as Product[]}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
