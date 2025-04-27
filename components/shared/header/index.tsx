2def34aab1a0284a916365dc966657068963c23e
Begriper <fero.stolar@gmail.com>
2025-04-27 19:13:45 +0200

    Search Component

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,5 +1,6 @@
 import CategoryDrawer from "@/components/shared/header/category-drawer";
 import Menu from "@/components/shared/header/menu";
+import Search from "@/components/shared/header/search";
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
@@ -22,6 +23,9 @@ const Header = () => {
               {APP_NAME}
             </span>
           </Link>
+        </div>
+        <div className="hidden md:block">
+          <Search />
         </div>
         <Menu />
       </div>
